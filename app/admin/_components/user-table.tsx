84f049eb8d558da9fbd9e6b5c0ce58728a0fd035
koda-mhel <mhel.tagpuno@kodakollectiv.com>
2024-10-14 14:36:00 +0800

    Feat: added layout and basic design

---
+++ app/admin/_components/user-table.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+
+export default function UserTable() {
+  const data = [
+    { name: "Tony Reichert", position: "CEO", status: "Active" },
+    { name: "Zoey Lang", position: "Technical Lead", status: "Paused" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    { name: "Jane Fisher", position: "Senior Developer", status: "Active" },
+    {
+      name: "William Howard",
+      position: "Community Manager",
+      status: "Vacation",
+    },
+  ];
+  return (
+    <Table
+      isHeaderSticky
+      removeWrapper
+      aria-label="Example static collection table"
+      classNames={{
+        wrapper:
+          "p-0 rounded-sm bg-black after:bg-foreground after:text-background text-background ",
+        th: "  rounded-none first:rounded-none last:rounded-none ",
+        tbody: "divide-y divide-gray-800",
+        base: "h-[calc(100vh-110px)] overflow-y-auto",
+      }}
+    >
+      <TableHeader>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>ROLE</TableColumn>
+        <TableColumn>STATUS</TableColumn>
+      </TableHeader>
+
+      <TableBody>
+        {data.map((row, index) => (
+          <TableRow key={index}>
+            <TableCell>{row.name}</TableCell>
+            <TableCell>{row.position}</TableCell>
+            <TableCell>{row.status}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
---
+++ app/admin/_sections/side-bar.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {
+  Avatar,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import Link from "next/link";
+import { FiHome, FiUsers } from "react-icons/fi";
+import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
+
+export default function SideBar() {
+  return (
+    <aside className="w-[200px] lg:w-[300px] flex-col transition-transform -translate-x-full bg-white border-r border-gray-800 sm:translate-x-0 dark:bg-black hidden md:flex">
+      <div className="h-16 flex items-center px-4">Logo</div>
+
+      <div className="flex flex-col px-4 py-2 justify-between h-full">
+        <div className=" overflow-y-auto bg-white dark:bg-black">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <Link
+                href="/admin"
+                className="flex items-center text-black rounded-lg dark:text-white hover:bg-black dark:hover:bg-black group"
+              >
+                <FiHome />
+                <span className="ms-3">Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/admin/users"
+                className="flex items-center text-black rounded-lg dark:text-white hover:bg-black dark:hover:bg-black group"
+              >
+                <FiUsers />
+                <span className="ms-3">Users</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Divider className="my-1.5" />
+          <Dropdown
+            classNames={{ content: "lg:min-w-[270px] min-w-[170px] rounded" }}
+          >
+            <DropdownTrigger>
+              <div
+                className="flex items-center gap-2  p-2"
+                aria-expanded="false"
+              >
+                <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+
+                <label>Registrar</label>
+                <PiDotsThreeOutlineVerticalFill className="ml-auto" />
+              </div>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Static Actions">
+              <DropdownItem key="new">New file</DropdownItem>
+              <DropdownItem key="copy">Copy link</DropdownItem>
+              <DropdownItem key="edit">Edit file</DropdownItem>
+              <DropdownItem key="delete" className="text-danger" color="danger">
+                Delete file
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </div>
+    </aside>
+  );
+}
